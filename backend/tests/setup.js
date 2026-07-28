eecd62c330df602f669d333ed7d972b78ac8017b
Javed Hallikeri <js21022102@gmail.com>
2026-07-28 12:48:18 +0530

    test: defer Fastify app.inject wrapping to prevent config mock pollution in unit tests

--- backend/tests/setup.js
+++ backend/tests/setup.js
@@ -29,19 +29,22 @@ jest.spyOn(console, 'warn').mockImplementation(() => {});
 jest.spyOn(console, 'error').mockImplementation(() => {});
 
 // Wrap Fastify inject to supply default Origin header matching config
-const app = require('../src/app');
-const originalInject = app.inject;
-app.inject = function (opts) {
-  if (opts) {
-    if (!opts.headers) {
-      opts.headers = {};
+jest.mock('../src/app', () => {
+  const actualApp = jest.requireActual('../src/app');
+  const originalInject = actualApp.inject;
+  actualApp.inject = function (opts) {
+    if (opts) {
+      if (!opts.headers) {
+        opts.headers = {};
+      }
+      const hasOrigin = Object.keys(opts.headers).some(
+        (key) => key.toLowerCase() === 'origin'
+      );
+      if (!hasOrigin) {
+        opts.headers['Origin'] = 'http://localhost:5173';
+      }
     }
-    const hasOrigin = Object.keys(opts.headers).some(
-      (key) => key.toLowerCase() === 'origin'
-    );
-    if (!hasOrigin) {
-      opts.headers['Origin'] = 'http://localhost:5173';
-    }
-  }
-  return originalInject.call(app, opts);
-};
+    return originalInject.call(actualApp, opts);
+  };
+  return actualApp;
+});
